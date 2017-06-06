786ff1b3a3bd95c4d498f1dbda6c9c36122b9e92
Benjamin Wellington <benhwell@gmail.com>
2017-06-06 15:22:32 -0400

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+const path = require("path");
+
+module.exports = {
+    entry: {
+         map: "./components/js/index.js"
+    },
+    output: {
+        path: path.join(__dirname, 'example'),
+        publicPath:"/example",
+        filename: "[name].js"
+    },
+    watch: true,
+    devtool: "inline-source-map",
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                enforce: "pre",
+                exclude: /node_modules/,
+                loader: "eslint-loader"
+            },
+            { 
+              test: /\.js$/, 
+              exclude: [/node_modules/], 
+              loader: "babel-loader",
+                query: {
+                    presets: ["es2015"]
+                } 
+            },
+            {
+              test: /\.css$/, 
+              loaders: ["style-loader","css-loader"] 
+            },
+            {
+            test: /\.scss$/,
+            loaders: ["style-loader", "css-loader", "sass-loader"]
+        },
+            { 
+                test: /\.(png|woff|woff2|eot|ttf|svg)$/,
+                loader: 'url-loader?limit=100000' 
+            },
+            {
+                test: /\.html/,
+                exclude: [/node_modules/],
+                loader: "raw-loader"
+            }
+        ]
+    }
+}
